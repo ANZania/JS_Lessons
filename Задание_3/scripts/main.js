626a728fd9459237593b133a16e696e4adc50fc7
ANZania <zianastasia@yandex.ru>
2020-08-10 18:14:37 +0300

    Исправлено

--- Задание_3/scripts/main.js
+++ Задание_3/scripts/main.js
@@ -7,7 +7,7 @@ let namePerson = 'Максим';
 const desicionA = (lang) => {  
     if (lang === 'ru') {
         console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота');
-    } else if (lang == 'en') {
+    } else if (lang === 'en') {
         console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
     } else {
         console.log('Что-то пошло не так');
