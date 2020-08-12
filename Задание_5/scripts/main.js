22eea34714c830ff1f0a06bd8a743fde36c3cabe
ANZania <zianastasia@yandex.ru>
2020-08-12 16:17:46 +0300

    Add Lesson_05 folder

---
+++ Задание_5/scripts/main.js
@@ -0,0 +1,33 @@
+'use strict'
+
+
+// Задание 1
+
+let arr = ['2345', '1234', '5000', '28', '6721334', '44065', '2144'];
+let result = [];
+
+for ( let i = 0; i < arr.length; i++ ) {
+    let firstNum = arr[i][0];
+
+    if ( firstNum === '2' || firstNum === '4' ) {
+        result.push( arr[i] );
+    }
+};
+
+console.log( result.join( ', ' ) );
+
+
+// Задание 2
+
+for ( let number = 1; number < 101; number++ ) {
+    let multiplier = 2;
+
+    for ( multiplier; multiplier < number; multiplier++ ) {
+        if ( number % multiplier === 0) {
+            break;
+        };
+    };
+    if ( multiplier === number && multiplier != 2) {
+        console.log( number );
+    };
+}
