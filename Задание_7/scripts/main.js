205d9bc6a05e6fd71d7871cb4f83d13946bfd648
ANZania <58665351+ANZania@users.noreply.github.com>
2020-08-16 14:12:07 +0300

    Fix document output

--- Задание_7/scripts/main.js
+++ Задание_7/scripts/main.js
@@ -4,18 +4,23 @@ let week = [ 'Понедельник', 'Вторник', 'Среда', 'Четв
 
 const now = new Date(); 
 const currentDay = now.getDay() - 1;
+const block = document.querySelector('.text-block');
 
 const main = function () {
     for ( let day in week ) {
-        console.log(day);
-        if ( week[day] === week[currentDay] && ( day === '5' || day === '6' + '\n') ) {
-            document.write ( '<pre>' +  week[day].italics().bold()  + '\n' + '<\/pre>');
-        } else if ( week[day] === week[currentDay] ) {
-            document.write ('<pre>' +   week[day].bold()  + '\n' + '<\/pre>');
+        console.log( week[day]);
+        if ( week[day] == week.slice(currentDay) && ( day === '5' || day === '6')) {
+            let content = '<pre>' +  (week[day].italics()).bold()  + '\n' + '<\/pre>';
+            block.insertAdjacentHTML('beforeend', content);
+        } else if ( (week[day] === week[currentDay]) ) {
+            let content = '<pre>' +   week[day].bold()  + '\n' + '<\/pre>'
+            block.insertAdjacentHTML('beforeend', content);
         } else if ( day === '5' || day === '6' ) {
-            document.write ('<pre>' +   week[day].italics() + '\n' + '<\/pre>' );
+            let content = '<pre>' +   week[day].italics() + '\n' + '<\/pre>';
+            block.insertAdjacentHTML('beforeend', content);
         } else {
-            document.write('<pre>' +  week[day] + '\n' + '<\/pre>');
+            let content = '<pre>' +  week[day] + '\n' + '<\/pre>';
+            block.insertAdjacentHTML('beforeend', content);
         };
     };
 };
