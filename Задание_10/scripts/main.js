4468050237b482f9741ab851abeebc5353ff7475
ANZania <zianastasia@yandex.ru>
2020-08-19 20:35:54 +0300

    Fix book replacing methods

--- Задание_10/scripts/main.js
+++ Задание_10/scripts/main.js
@@ -9,28 +9,19 @@ const book3 = document.querySelectorAll('.book')[4];
 const book4 = document.querySelectorAll('.book')[3];
 const book5 = document.querySelectorAll('.book')[5];
 const book6 = document.querySelectorAll('.book')[2];
-const head3 = book3.getElementsByTagName('h2')[0];
+const head3 = book3.querySelector('h2');
 
-const chapters2 = book2.getElementsByTagName('ul');
-const chapters5 = book5.getElementsByTagName('ul');
-const chapters6 = book6.getElementsByTagName('ul');
+const chapters2 = book2.querySelectorAll('ul');
+const chapters5 = book5.querySelectorAll('ul');
+const chapters6 = book6.querySelectorAll('ul');
 
 const paragraph = document.createElement('li');
 paragraph.textContent = 'Глава 8: За пределами ES6';
 
-book1.remove();
-book2.remove();
-book3.remove();
-book4.remove();
-book5.remove();
-book6.remove();
-
-booksCollection.insertAdjacentElement('beforeend', book1);
-booksCollection.insertAdjacentElement('beforeend', book2);
-booksCollection.insertAdjacentElement('beforeend', book3);
-booksCollection.insertAdjacentElement('beforeend', book4);
-booksCollection.insertAdjacentElement('beforeend', book5);
-booksCollection.insertAdjacentElement('beforeend', book6);
+booksCollection.children[1].after(book2);
+booksCollection.children[1].after(book3);
+booksCollection.children[2].after(book4);
+booksCollection.children[3].after(book5);
 
 chapters2[0].children[9].after(chapters2[0].children[2]);
 chapters2[0].children[7].after(chapters2[0].children[3]);
