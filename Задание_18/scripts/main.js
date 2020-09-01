a50c616d9e2cc0c64e0165bfff2471743ccaa592
ANZania <58665351+ANZania@users.noreply.github.com>
2020-09-01 17:34:41 +0300

    Update main.js

--- Задание_18/scripts/main.js
+++ Задание_18/scripts/main.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', function () {
     'use strict'
 
     //Timer
-    const countTimer (deadline) => {
+    const countTimer = (deadline) => {
         let timerHours = document.getElementById('timer-hours'),
             timerMinutes = document.getElementById('timer-minutes'),
             timerSeconds = document.getElementById('timer-seconds'); 
 
-        const getTimeRemaining () => {
+        const getTimeRemaining = () => {
             let dateStop = new Date(deadline).getTime(),
                 dateNow = new Date().getTime(),
                 timeRemaining = (dateStop - dateNow) / 1000,
@@ -28,14 +28,14 @@ window.addEventListener('DOMContentLoaded', function () {
                 return {timeRemaining, hours, minutes, seconds};
         }
 
-        const addZero (number) => {
+        const addZero = (number) => {
             if (number < 10) {
                 number = '0' + number;
             }
             return number;
         };
 
-        const updateClock () => {
+        const updateClock = () => {
             let timer = getTimeRemaining();
             timerHours.textContent = addZero(timer.hours);
             timerMinutes.textContent = addZero(timer.minutes);
