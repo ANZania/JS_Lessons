5745ff713954b2c4d34c58b62de92e3a03e96c61
ANZania <58665351+ANZania@users.noreply.github.com>
2020-09-17 21:23:15 +0300

    Add Lesson_28 folder

---
+++ ajaxCar/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    const addResponse = () => {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', './cars.json');
+            request.setRequestHeader('Content-type', 'application/json');
+            request.send();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+                        const data = JSON.parse(request.responseText);
+                        resolve(data);
+                    } else {
+                        reject();
+                    }
+                }
+                
+            }); 
+        });
+    };
+
+    select.addEventListener('change', () => {
+        addResponse()
+            .then((data) => {
+                data.cars.forEach(item => {
+                    if (item.brand === select.value) {
+                        const {brand, model, price} = item;
+                        output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+                    }
+                });
+            })
+            .catch(() => {
+            output.innerHTML = 'Произошла ошибка';
+            });
+    });
+
+});
